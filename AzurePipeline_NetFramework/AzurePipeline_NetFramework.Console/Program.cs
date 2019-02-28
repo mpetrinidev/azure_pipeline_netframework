cbdf866454b4288e24bbf76192a59dd20c59ddb6
mpetrini <dev.mpetrini@gmail.com>
2019-02-28 20:43:03 -0300

    add console app

---
+++ AzurePipeline_NetFramework/AzurePipeline_NetFramework.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AzurePipeline_NetFramework.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Saludos desde mi consola");
+            System.Console.ReadLine();
+        }
+    }
+}
